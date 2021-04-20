de268786fd125977d12d7d1148a7db71b4083c09
Ashnaz <ashnaz@iposg.com>
2021-04-20 14:35:17 +0530

    Add existing file

--- pages/issue/create.tsx
+++ pages/issue/create.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
+interface IssueI {
+    id: number;
+    custno: number;
+    issueType: string;
+    create: string;
+}
+
 const create = () => {
+    const [state, setstate] = React.useState<IssueI>({
+        id: 0,
+        custno: 0,
+        issueType: '',
+        create: ''
+    })
     return (
         <div>
             <h1>create</h1>
